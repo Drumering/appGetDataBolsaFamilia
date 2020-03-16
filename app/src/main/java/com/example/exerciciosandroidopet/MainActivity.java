fbc43ab22fa07619b1d10b5f60e64f5c4fa67983
EgonStep <041957Ab@>
2020-03-16 10:44:03 -0300

    Criado Request para o site do IBGE

--- app/src/main/java/com/example/exerciciosandroidopet/MainActivity.java
+++ app/src/main/java/com/example/exerciciosandroidopet/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.exerciciosandroidopet;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -21,13 +23,16 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private EditText editMunicipio;
     private List<String> names;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textName);
 
+        textView = findViewById(R.id.textName);
+        editMunicipio = findViewById(R.id.editMunicipio);
         names = new ArrayList<>();
     }
 
@@ -35,26 +40,30 @@ public class MainActivity extends AppCompatActivity {
         carregarDados();
     }
 
-    private void generateRequest(String url){
-        //url = url + "mesAno=201811&codigoIbge=4106902&pagina=1";
-        String endpoint = url;
+    public void btnCarregarIBGEEvent(View v) {
+        carregarCodigoIBGE();
+    }
 
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, endpoint, null, new Response.Listener<JSONObject>() {
+    private void generateRequest(String url) {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+
             @Override
             public void onResponse(JSONObject response) {
-                if(response.has("results")){
+                if (response.has("results")) {
                     try {
                         JSONArray array = response.getJSONArray("results");
+
                         for(int i = 0; i < array.length(); i++){
                             names.add(array.getJSONObject(i).getString("name"));
                         }
-                        if(response.has("next")){
+
+                        if (response.has("next")) {
                             String url = response.getString("next");
                             if(!url.equals("null")){
                                 Log.i("VOLLEY",url);
                                 generateRequest(url);
-                            }
-                            else{
+                            } else {
                                 Log.i("VOLLEY","ENCERRADO");
                                 showData();
                             }
@@ -65,27 +74,54 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
 
-            }
+            @Override
+            public void onErrorResponse(VolleyError error) { }
         });
 
         APISingleton.getInstance(this).addToRequestQueue(request);
     }
 
-    private void showData() {
-        String names_str = "";
-        for(String name : names){
-            names_str += name + "\n";
-        }
-        textView.setText(names_str);
+    private void generateRequestIBGE(String url) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            editMunicipio.setText(response.get("id").toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) { }
+        });
+
+        APISingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 
-    private void carregarDados(){
-        String endpoint = "https://pokeapi.co/api/v2/pokemon";
-        //String endpoint = "http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio?";
+    private void showData() {
+        StringBuilder names_str = new StringBuilder();
+        for (String name : names) {
+            names_str.append(name).append("\n");
+        }
 
+        textView.setText(names_str.toString());
+    }
+
+    private void carregarDados() {
+        String endpoint = "http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio?";
         generateRequest(endpoint);
+    }
+
+    private void carregarCodigoIBGE() {
+        String cidade = editMunicipio.getText().toString().replace(' ', '-');
+
+        String endpoint = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/" +
+                cidade;
+        generateRequestIBGE(endpoint);
     }
 }
